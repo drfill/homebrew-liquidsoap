818d337f7389b9702bfaa7e35e3b5c690802a0bf
Fill Q <admin@njoyx.net>
2012-05-20 19:59:45 +0400

    video processing argument bug

--- Formula/liquidsoap.rb
+++ Formula/liquidsoap.rb
@@ -36,7 +36,7 @@ class Liquidsoap < Formula
   depends_on "soundtouch" if ARGV.include? "--with-soundtouch" and Hardware.is_64_bit?
   depends_on "libaacplus" if ARGV.include? "--with-aacplus"
   depends_on "vo-aacenc" if ARGV.include? "--with-aac"
-  depends_on "camlimages" if ARGV.include? "--with-video"
+  depends_on "camlimages" if ARGV.include? "--with-video-processing"
 
   def options
     [['--with-samplerate', "Enables Samplerate library support"],
