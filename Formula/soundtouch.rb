df3a9912357491c7727a8993e5eef1da729b975c
Vadims I <vadim@kramer.su>
2012-05-21 18:02:50 +0400

    bootstrap fail if glib tool not installed

--- Formula/soundtouch.rb
+++ Formula/soundtouch.rb
@@ -7,12 +7,13 @@ class Soundtouch < Formula
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
+  depends_on 'libtool' => :build
 
   def install
     # The build fails complaining about out of date libtools. Rerunning the autoconf from bootstrap prevents the error.
     system "sh", "bootstrap"
 
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--enable-shared", "--disable-static"
     system "make install"
   end
 end
