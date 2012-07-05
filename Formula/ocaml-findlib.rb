4e9bd87aead6f176add4ee0a456d65c6ccbd4a4b
Vadims I <vadim@kramer.su>
2012-07-05 21:16:43 +0400

    ~+ ocaml-findlib config error and new version ~ ocaml-syslog download error due to closing Apple's mobile me

--- Formula/ocaml-findlib.rb
+++ Formula/ocaml-findlib.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class OcamlFindlib < Formula
   homepage 'http://projects.camlcity.org/projects/findlib.html'
-  url 'http://download.camlcity.org/download/findlib-1.3.1.tar.gz'
-  md5 'e632bad87f1c7be9414a6b754232ba01'
+  url 'http://download.camlcity.org/download/findlib-1.3.2.tar.gz'
+  md5 '672e3a644015dda74daf89b7fcdec904'
 
   depends_on 'objective-caml' => :build
 
   def install
-    system "./configure", "-config #{etc}/findlib.conf", "-sitelib #{HOMEBREW_PREFIX}/lib/ocaml/site-lib", "-system osx", "-bindir #{bin}", "-mandir #{man}", "-with-toolbox"
+    system "./configure -config #{etc}/findlib.conf -sitelib #{HOMEBREW_PREFIX}/lib/ocaml/site-lib -system osx -bindir #{bin} -mandir #{man} -with-toolbox"
     system 'make'
     system 'make opt'
     mkdir_p "#{lib}/ocaml/site-lib"
--- Formula/ocaml-syslog.rb
+++ Formula/ocaml-syslog.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class OcamlSyslog < Formula
   homepage 'http://homepage.mac.com/letaris/'
-  url 'http://homepage.mac.com/letaris/syslog-1.4.tar.gz'
+  url 'http://ftp.de.debian.org/debian/pool/main/s/syslog-ocaml/syslog-ocaml_1.4.orig.tar.gz'
   md5 '3042185e6f511aea9956cd8f172b1a84'
+  version '1.4'
 
   depends_on 'objective-caml' => :build
   depends_on 'ocaml-findlib' => :build
