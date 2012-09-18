617b929bde219838dd096ca910f84fed37a72ea0
Romain Beauxis <toots@rastageeks.org>
2012-09-18 18:17:45 +0200

    Add missing ocaml-idl META file and install all it's library files under site-lib.

--- Formula/ocaml-idl.rb
+++ Formula/ocaml-idl.rb
@@ -12,9 +12,20 @@ class OcamlIdl < Formula
     cp "config/Makefile.unix", "config/Makefile"
     inreplace "config/Makefile", "OCAMLLIB=/usr/local/lib/ocaml", "OCAMLLIB=#{lib}/ocaml"
     inreplace "config/Makefile", "BINDIR=/usr/local/bin", "BINDIR=#{bin}"
+    inreplace "lib/Makefile", "$(OCAMLLIB)", "$(OCAMLLIB)/site-lib/camlidl" 
     system "make all"
     mkdir_p "#{bin}"
     mkdir_p "#{lib}/ocaml/caml"
+    mkdir_p "#{lib}/ocaml/site-lib/camlidl"
     system "make install"
+    File.open "#{lib}/ocaml/site-lib/camlidl/META", "w" do |f|
+      f.write <<END
+description = "Stub generator"
+version = "1.05"
+requires = ""
+archive(byte) = "com.cma"
+archive(native) = "com.cmxa"
+END
+    end
   end
 end
