ebd7f3d211310d980e03e87550121c152ecfeb95
Romain Beauxis <toots@rastageeks.org>
2016-01-26 13:41:06 -0600

    Fix shasum.

--- Formula/ladspa_header.rb
+++ Formula/ladspa_header.rb
@@ -3,7 +3,7 @@ require 'formula'
 class LadspaHeader < Formula
   homepage 'http://www.ladspa.org/'
   url 'http://www.ladspa.org/ladspa_sdk/ladspa.h.txt'
-  sha256 'c72ceb7383f159a944bfe80b1b155795857026aea1155dbe4ecf1664354320ad'
+  sha256 '1bd380baaf018be5e05323c192444adf058ed8a35b8cbf4c8a11519fc81cb036'
   version '1.1'
 
   def install
