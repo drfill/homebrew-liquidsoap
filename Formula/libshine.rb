2810d0d25958da15fa1f3d2f8dab9c45dfa9532b
Romain Beauxis <toots@rastageeks.org>
2017-08-04 17:44:36 -0500

    Update libshine to 3.1.1

--- Formula/libshine.rb
+++ Formula/libshine.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libshine < Formula
   homepage 'http://liquidsoap.fm/'
-  url 'http://downloads.sourceforge.net/project/savonet/shine/shine-2.0.0.tar.gz'
-  sha256 '54dc2219582e476ff854519fa1d7f9560323132f24b0ffb77dcf0da812edd5db'
+  url 'https://github.com/toots/shine/releases/download/3.1.1/shine-3.1.1.tar.gz'
+  sha256 '58e61e70128cf73f88635db495bfc17f0dde3ce9c9ac070d505a0cd75b93d384'
 
   def install
     system "./configure", "--prefix=#{prefix}"
